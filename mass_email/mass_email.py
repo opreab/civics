22cc569f8a65e623eb233f12c6836c9c0d133c1c
opreab <opreamarius14@gmail.com>
2017-01-17 18:54:59 +0200

    mass_email: added configurator, encapsulated server operations in class, changes untested

--- mass_email/mass_email.py
+++ mass_email/mass_email.py
@@ -1,12 +1,47 @@
+import sys
 import smtplib
-fromaddr = '@gmail.com'
-toaddrs  = '@gmail.com'
-msg = 'Why,Oh why!'
-username = '@gmail.com'
-password = 'pwd'
-server = smtplib.SMTP('smtp.gmail.com:587', timeout=10)
-server.ehlo()
-server.starttls()
-server.login(username,password)
-server.sendmail(fromaddr, toaddrs, msg)
-server.quit()
+import ConfigParser
+
+if len(sys.argv) != 2:
+    print "Error: please provide config ini file!")
+    sys.exit(1)
+
+cfg = ConfigParser.ConfigParser()
+cfg.read(sys.argv[1])
+
+
+class EmailServer(object):
+	def __init__(url, user, pwd)
+		self.url = url
+		self.user = user
+		serf.pwd = pwd
+		self.timeout = 10
+		self.server = None
+		self.connect()
+
+	def connect(self):
+		if not self.server:
+			try:
+				self.server = smtplib.SMTP(self.url, timeout=self.timeout)
+				self.server.ehlo()
+				self.server.starttls()
+				self.server.login(self.user, self.pwd)
+			except:
+				self.server = None
+				raise
+	
+	def terminate(self):
+		if self.server:
+			self.server.quit()
+
+	def send_email(self, from, to, msg):
+		try:
+			self.server.sendmail(fromaddr, toaddrs, msg)
+		except:
+			raise
+
+def main():
+	
+
+if __name__ == '__main__':
+	main()
